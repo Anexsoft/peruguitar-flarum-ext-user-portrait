2daa4ceb9b55d90dbc760998c1672a29774863fe
Eduardo <eduardo.rodriguez01@globant.com>
2023-02-15 18:15:06 -0500

    feat: enable/disable option for mobile

--- js/src/forum/components/UserPortrait.jsx
+++ js/src/forum/components/UserPortrait.jsx
@@ -1,17 +1,18 @@
 export default function addUserPortrait() {
-  const user = app.session.user.data.attributes;
+    const availableForMobile = app.forum.attribute('pgUserPortraitAvailableForMobile') === 1;
+    const user = app.session.user.data.attributes;
 
-  return (
-    <div class="pg-user-portrait">
-      <div class="pg-user-portrait-avatar">
-        {user.avatarUrl && (<img src={user.avatarUrl} />)}
-        {!user.avatarUrl && (<span>{user.displayName[0]}</span>)}
-      </div>
+    return (
+        <div className={`pg-user-portrait ${!availableForMobile ? 'pg-user-portrait-mobile-hidden' : ''}`}>
+            <div class="pg-user-portrait-avatar">
+                {user.avatarUrl && <img src={user.avatarUrl} />}
+                {!user.avatarUrl && <span>{user.displayName[0]}</span>}
+            </div>
 
-      <div className="pg-user-portrait-username">
-        <span>{app.translator.trans('peruguitar-user-portrait.forum.welcome_back_label')},</span>
-        <span>{user.displayName}</span>
-      </div>
-    </div>
-  );
+            <div className="pg-user-portrait-username">
+                <span>{app.translator.trans('peruguitar-user-portrait.forum.welcome_back_label')},</span>
+                <span>{user.displayName}</span>
+            </div>
+        </div>
+    );
 }
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,11 +5,11 @@ import IndexPage from 'flarum/components/IndexPage';
 import addUserPortrait from './components/UserPortrait';
 
 app.initializers.add('peruguitar/user-portrait', () => {
-  extend(IndexPage.prototype, 'sidebarItems', (items) => {
-    if (!app.session.user) {
-      return;
-    }
+    extend(IndexPage.prototype, 'sidebarItems', (items) => {
+        if (!app.session.user) {
+            return;
+        }
 
-    items.add('UserPortrait', addUserPortrait(), 1);
-  });
+        items.add('UserPortrait', addUserPortrait(), 1);
+    });
 });
